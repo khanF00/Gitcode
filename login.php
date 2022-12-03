ca8b97b5f4074391062ac072563cc0ccca7046e5
osahenry11 <84633397+osahenry11@users.noreply.github.com>
2022-12-03 14:47:17 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,8 +1,10 @@
+
 <?php
+
     $servername='localhost';
     $username='root';
     $password='';
-    $dbname = "reviews";
+    $dbname = "login";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
@@ -12,16 +14,14 @@
 
 if(isset($_POST['submit']))
 {    
-$username = $_POST['username'];
-$restuarant = $_POST['restuarant'];
-$rating = $_POST['rating'];
-$title = $_POST['title'];
-$pros = $_POST['pros'];
-$cons = $_POST['cons'];
-$review = $_POST['review'];
-$recommend = $_POST['recommend'];
-     $sql = "INSERT INTO register (username,restuarant,rating,title,pros,cons,review,recommend)
-     VALUES ('$username','$restuarant','$rating','$title','$pros','$cons', '$review','$recommend')";
+        $name = $_POST['name'];
+        $panther = $_POST['panther'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $checkbox = $_POST['checkbox'];
+     $sql = "INSERT INTO register (name,panther,email,username,password,checkbox)
+     VALUES ('$name','$panther','$email','$username','$password','$checkbox')";
      if (mysqli_query($conn, $sql)) {
         echo "New record has been added successfully !";
      } else {
